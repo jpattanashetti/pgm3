936c7c76ebc28c11e6f2e1bbd5bf5a01d92626c1
jpattanashetti <jpattanashetti9@gmail.com>
2025-05-06 11:43:42 +0530

    java hee hee

---
+++ Person.java
@@ -0,0 +1,16 @@
+package pgm3;
+
+public class Person {
+
+    String name;
+    String dob;
+    public Person(String name, String dob) {
+        this.name = name;
+        this.dob = dob;
+    }
+    
+    public void displayPerson(){
+        System.out.println("Name: " + name);
+        System.out.println("date of birth: " + dob);
+    }
+}
